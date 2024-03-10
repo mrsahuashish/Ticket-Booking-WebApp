97e40404cc37dd4346c1097cbb4232e4244813d4
Ashish Sahu <mrsahuashish@gmail.com>
2024-03-10 17:13:28 +0530

    Add initial commit

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import Homeslider from "../components/Homeslider"
+
+const About = () => {
+  return (
+    <>
+        <Homeslider title={"OUR STORY"} imageUrl ={"/about1.svg"} />
+    </>
+  )
+}
+
+export default About
---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+import Homeslider from "./components/Homeslider"
+const page = () => {
+  return (
+    <Homeslider title={"LET\'S WATCH MOVIE TOGETHER"} imageUrl={"/home.svg"}/>
+  )
+}
+
+export default page
